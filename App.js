57336e57c93a9b8ce723512b039dcfccedf5edf1
PhuongThao15 <tieuhivong7201@gmail.com>
2022-10-27 10:18:50 +0700

    finish 1

--- App.js
+++ App.js
@@ -1,20 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useRef, useEffect } from "react";
+import {
+  Animated,
+  Text,
+  View,
+  StyleSheet,
+  Button,
+  SafeAreaView,
+  StatusBar,
+  PanResponder,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
-export default function App() {
+const App = () => {
+  const move = useRef(new Animated.Value(0)).current;
+
+  const fadeAnim = useRef(new Animated.Value(0)).current;
+  useEffect(() => {
+    Animated.timing(fadeAnim, {
+      toValue: 1,
+      duration: 2000,
+    }).start();
+  }, [fadeAnim]);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.AndroidSafeArea}>
+      <View style={styles.container}>
+
+        <Animated.View
+          style={[
+            styles.fadingContainer,
+            {
+              opacity: fadeAnim,
+            },
+          ]}
+        >
+          <Text style={styles.text}>
+            welcome to Animation React Native
+          </Text>
+        </Animated.View>
+
+      </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
+  AndroidSafeArea: {
+    flex: 1,
+    backgroundColor: "white",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+  },
+
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+
+
+  fadingContainer: {
+    padding: 20,
+    backgroundColor: "violet",
+  },
+  text: {
+    fontSize:20 ,
+  },
+
 });
+
+export default App;
